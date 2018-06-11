6ac2e390faddfdbe76aee49f2a0c690a6512df86
kagsburg <kagsburg@gmail.com>
2018-06-11 12:00:10 -0700

    adjusted code and history

--- linear.R
+++ linear.R
@@ -43,7 +43,7 @@ model_p <- pf(f[1], f[2], f[3], lower=FALSE)
 p_value <- 2*pt(-abs(t_value), df=nrow(cars)-ncol(cars))  # calc p Value
 f_statistic <- linearMod$fstatistic[1]  # fstatistic
 f <- summary(linearMod)$fstatistic  # parameters for model p-value calc
-model_p <- pf(f[1], f[2], f[3], lower=FALSE)
+#model_p <- pf(f[1], f[2], f[3], lower=FALSE)
 
 
 
